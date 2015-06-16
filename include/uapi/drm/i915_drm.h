0c6f9006c05995e422225df0a3604071e5088f67
Robert Bragg <robert@sixbynine.org>
2015-06-16 15:09:28 +0100

    drm/i915: expose _SUBSLICE_MASK GETPARM
    Assuming a uniform mask across all slices, this enables userspace to
    determine the specific sub slices enabled. This information is required,
    for example, to be able to analyse some OA counter reports where the
    counter configuration depends on the HW sub slice configuration.
    
    Update: new parameter number
    
    Signed-off-by: Robert Bragg <robert@sixbynine.org>

--- include/uapi/drm/i915_drm.h
+++ include/uapi/drm/i915_drm.h
@@ -360,6 +360,7 @@ typedef struct drm_i915_irq_wait {
 #define I915_PARAM_HAS_RESOURCE_STREAMER 36
 #define I915_PARAM_HAS_EXEC_SOFTPIN	 37
 #define I915_PARAM_SLICE_MASK		 38
+#define I915_PARAM_SUBSLICE_MASK	 39
 
 typedef struct drm_i915_getparam {
 	__s32 param;
