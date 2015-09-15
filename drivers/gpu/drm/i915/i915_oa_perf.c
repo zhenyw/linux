ce19c307b8b9a064fbcbf6b18ba1ee8936f76f0c
Zhenyu Wang <zhenyuw@linux.intel.com>
2015-09-15 15:59:55 +0800

    drm/i915: Add compute basic metric config for BDW
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>

--- drivers/gpu/drm/i915/i915_oa_perf.c
+++ drivers/gpu/drm/i915/i915_oa_perf.c
@@ -704,6 +704,27 @@ static void bdw_enable_metric_set(struct perf_event *event)
 		dev_priv->oa_pmu.flex_regs = i915_oa_3d_flex_eu_config_bdw;
 		dev_priv->oa_pmu.flex_regs_len = i915_oa_3d_flex_eu_config_bdw_len;
 		break;
+	case I915_OA_METRICS_SET_COMPUTE:
+		if (INTEL_INFO(dev_priv)->slice_mask & 0x1) {
+			dev_priv->oa_pmu.mux_regs =
+				i915_oa_compute_mux_config_1_0_slice_mask_0x01_bdw;
+			dev_priv->oa_pmu.mux_regs_len =
+				i915_oa_compute_mux_config_1_0_slice_mask_0x01_bdw_len;
+		} else if (INTEL_INFO(dev_priv)->slice_mask & 0x2) {
+			dev_priv->oa_pmu.mux_regs =
+				i915_oa_compute_mux_config_1_2_slice_mask_0x02_bdw;
+			dev_priv->oa_pmu.mux_regs_len =
+				i915_oa_compute_mux_config_1_2_slice_mask_0x02_bdw_len;
+		}
+
+		dev_priv->oa_pmu.b_counter_regs =
+			i915_oa_compute_b_counter_config_bdw;
+		dev_priv->oa_pmu.b_counter_regs_len =
+			i915_oa_compute_b_counter_config_bdw_len;
+
+		dev_priv->oa_pmu.flex_regs = i915_oa_compute_flex_eu_config_bdw;
+		dev_priv->oa_pmu.flex_regs_len = i915_oa_compute_flex_eu_config_bdw_len;
+		break;
 	default:
 		BUG(); /* should have been validated in _init */
 		return;
@@ -916,7 +937,8 @@ static int i915_oa_event_init(struct perf_event *event)
 	} else if (IS_BROADWELL(dev_priv->dev) || IS_CHERRYVIEW(dev_priv->dev) ||
 		   IS_SKYLAKE(dev_priv->dev))
 	{
-		if (oa_attr.metrics_set != I915_OA_METRICS_SET_3D)
+		if (oa_attr.metrics_set != I915_OA_METRICS_SET_3D &&
+			oa_attr.metrics_set != I915_OA_METRICS_SET_COMPUTE)
 			return -EINVAL;
 	} else {
 		BUG(); /* pmu shouldn't have been registered */
